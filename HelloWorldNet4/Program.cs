72818fb3f4a28f445d908d807f7c210a4974e012
efilnukefesin <efil.threat@googlemail.com>
2016-08-09 23:06:02 +0200

    Add files via upload
    initial commit

---
+++ HelloWorldNet4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldNet4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World in .NET 4");
+            Console.ReadKey();
+        }
+    }
+}
